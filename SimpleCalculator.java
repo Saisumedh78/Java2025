ab111d765a7fa20208688760e957bcaf309b8e5e
Saisumedh78 <saisumedhg16@gmail.com>
2025-06-19 23:38:29 +0530

    Simple Calculator

---
+++ SimpleCalculator.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        double num1 = input.nextDouble();
+
+        System.out.print("Enter an operator (+, -, *, /): ");
+        char operator = input.next().charAt(0);
+
+        System.out.print("Enter second number: ");
+        double num2 = input.nextDouble();
+
+        double result = 0;
+
+        if (operator == '+') {
+            result = num1 + num2;
+        } else if (operator == '-') {
+            result = num1 - num2;
+        } else if (operator == '*') {
+            result = num1 * num2;
+        } else if (operator == '/') {
+            if (num2 != 0) {
+                result = num1 / num2;
+            } else {
+                System.out.println("Cannot divide by zero.");
+                return;
+            }
+        } else {
+            System.out.println("Invalid operator.");
+            return;
+        }
+
+        System.out.println("Result: " + result);
+        input.close();
+    }
+}
--- program.java
+++
@@ -1,5 +0,0 @@
-public class program {
-    public static void main(String[] args) {
-        System.out.println("My Name is Sai Sumedh");
-    }
-}
